dc5c04435e42bc6df84a289a85320f81f49e2ab0
MaximeFth <maxime.fellrath@gmail.com>
2022-01-09 11:35:28 +0100

    add word correction

--- date.py
+++ date.py
@@ -4,10 +4,27 @@ import random
 import sqlite3
 import calendar
 from rich.console import Console
-
+import numpy as np
 
 weekdays = {1:'Monday',2:'Tuesday',3:'Wednesday',4:"Thursday",5:"Friday",6:"Saturday",0:"Sunday"}
 console = Console()
+def lcs(X, Y, m, n):
+  
+    if m == 0 or n == 0:
+       return 0;
+    elif X[m-1] == Y[n-1]:
+       return 1 + lcs(X, Y, m-1, n-1);
+    else:
+       return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+  
+  
+def closestTo(word):
+    dists = []
+    for i in range(7):
+        dists.append(lcs(weekdays[i].lower(),word.lower(),len(weekdays[i]), len(word)))
+    print(dists)
+    return weekdays[np.argmax(dists)]
+
 
 def dayToEng(myDate):
     date_suffix = ["th", "st", "nd", "rd"]
@@ -46,12 +63,17 @@ if __name__ == "__main__":
     newdate = gendate()
     print(dateToEng(newdate))
     answer = input("Enter day:")
-    answer = str(answer)
+    answer2 = str(answer)
+    answer = closestTo(answer2)
+    if(answer.upper() != answer2.upper()):
+        console.print("[+]", style = 'green', end='')
+        console.print(" Answer corrected: ",answer) 
     while weekdays.get((newdate.weekday()+1)%7).upper() != answer.upper():
         tries += 1
         print("Nope")
         answer = input("Enter day: ")
         answer = str(answer)
+        answer = closestTo(answer)
     t2 = t.time()
     print("**Congrats! Time: {:.2f}**".format(t2-t1))
     addToDB(t2-t1,newdate,tries)
